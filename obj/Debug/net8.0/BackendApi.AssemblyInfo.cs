a10c21f0cdc22b7815bf8ca2faf46fef9bb048d7
Samnang <samnangheang0@gmail.com>
2025-05-25 18:37:42 +0700

    Update DB

--- obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b27f5208d521244075883cd76b8c74b91d73637")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d53d0f73f9ac19e55a505fc1494dc0819cabcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
